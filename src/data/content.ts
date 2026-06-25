bb66ba48621d1314bd106817e2bf883d3e61ea2f
Felix Kirchner <felix.kirchner.fk@gmail.com>
2026-06-25 15:29:53 +0200

    Updated social link icons

--- src/data/content.ts
+++ src/data/content.ts
@@ -1,5 +1,6 @@
 import type {SocialLink, TreeLink} from "./types.ts";
-import {FaApple, FaInstagram, FaSoundcloud, FaSpotify, FaTiktok, FaYoutube} from "react-icons/fa";
+import {SiApplemusic, SiInstagram, SiSpotify, SiTiktok, SiYoutube} from "react-icons/si";
+import {FaGlobe} from "react-icons/fa";
 
 export const SPOTIFY_EMBED_URL: string = 'https://open.spotify.com/embed/artist/2ATI9IAeXPE31HKReWj40a?theme=0';
 
@@ -34,33 +35,31 @@ export const SOCIAL_LINKS: SocialLink[] = [
     {
         title: "Véloclub auf Instagram",
         url: "https://instagram.com/veloclubband",
-        icon: FaInstagram
+        icon: SiInstagram,
     },
     {
         title: "Véloclub auf Spotify",
         url: "https://open.spotify.com/artist/2ATI9IAeXPE31HKReWj40a",
-        icon: FaSpotify
-    },
-    {
-        title: "Véloclub auf Apple Music",
-        url: "https://music.apple.com/de/artist/v%C3%A9loclub/1706116595",
-        icon: FaApple
+        icon: SiSpotify,
     },
     {
         title: "Véloclub auf TikTok",
         url: "https://www.tiktok.com/@veloclubband",
-        icon: FaTiktok
+        icon: SiTiktok,
+    },
+    {
+        title: "Véloclub auf Apple Music",
+        url: "https://music.apple.com/de/artist/v%C3%A9loclub/1706116595",
+        icon: SiApplemusic,
     },
     {
         title: "Véloclub auf Youtube",
         url: "https://www.youtube.com/@veloclubband",
-        icon: FaYoutube
+        icon: SiYoutube,
     },
     {
-        title: "Véloclub auf Facebook",
-        url: "https://soundcloud.com/veloclubband",
-        icon: FaSoundcloud
+        title: "Webseite",
+        url: "https://www.veloclubband.de",
+        icon: FaGlobe,
     }
-
-
 ];
