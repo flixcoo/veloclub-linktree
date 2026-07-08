314a56fc027e0b2bdcc05afd54df2bff94b114aa
Felix Kirchner <felix.kirchner.fk@gmail.com>
2026-07-08 13:27:42 +0200

    Updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,34 +1,40 @@
 export function Footer() {
     return (
         <footer className={"flex flex-col items-center justify-center gap-5 text-xs text-gray-500"}>
-            <div className={"flex gap-4"}>
-                <a
-                    className={"text-black underline transition-all duration-300 hover:text-orange-500"}
-                    href={"https://veloclubband.de/impressum"}
-                >
-                    Impressum
-                </a>
+            <div className="flex flex-col items-center gap-1 text-xs text-gray-500">
+                {/* Design flag*/}
+                <span>
+                        Designed & developed by&nbsp;
+                    <a
+                        href="https://felixkirchner.de"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-black underline! transition-colors hover:text-orange-500"
+                    >
+                            Felix Kirchner
+                        </a>
+                        .
+                    </span>
 
-                <a
-                    className={"text-black underline transition-all duration-300 hover:text-orange-500"}
-                    href={"https://veloclubband.de/datenschutz"}
-                >
-                    Datenschutz
-                </a>
+                {/* Legal & Privacy */}
+                <div className={'flex items-center gap-4'}>
+                    <a
+                        className={"text-black underline transition-all duration-300 hover:text-orange-500"}
+                        href={"https://veloclubband.de/impressum"}
+                    >
+                        Impressum
+                    </a>
+                    <a
+                        className={"text-black underline transition-all duration-300 hover:text-orange-500"}
+                        href={"https://veloclubband.de/datenschutz"}
+                    >
+                        Datenschutz
+                    </a>
+                </div>
+
+                {/* Copyright */}
+                <span className={'mt-2'}>&copy; 2023 - {new Date().getFullYear()} Véloclub</span>
             </div>
-
-            <span>
-                Designed & developed by&nbsp;
-                <a
-                    href="https://felixkirchner.de"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-black underline transition-colors hover:text-orange-500"
-                >
-                    Felix Kirchner
-                </a>
-                .
-            </span>
         </footer>
     );
 }
